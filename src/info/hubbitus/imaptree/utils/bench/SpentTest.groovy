2e04d14ddd29fbe6594e7acf83ac61ae73c1f155
Hubbitus <pahan@hubbitus.info>
2015-01-04 19:34:01 +0300

    1) Make some classes static type checked (annotate @CompileStatic): Spent, Size (already was), ConfigExtended, ImapAccount 2) Import from ais adapter utils.bench package. Adopt and refactor Spent, ProgressLogger (expand docs, add examples) 3) Add progress logging into ImapTree. 4) Introduce first test - change project layout. But groovy-tests are not runned from Idea directly and JetBrains bugtracker https://youtrack.jetbrains.com/ down. See SpentTest.groovy for more details. 5) Add .idea directory under versioning control.

---
+++ src/info/hubbitus/imaptree/utils/bench/SpentTest.groovy
@@ -0,0 +1,36 @@
+package info.hubbitus.imaptree.utils.bench
+
+/**
+ * File generated and then run, but fails:
+ * /usr/lib/jvm/java-1.8.0-openjdk/bin/java -ea -Didea.launcher.port=7537 -Didea.launcher.bin.path=/home/_opt/JetBrains/idea-14.0.2-IC-139.659.2/bin -Dfile.encoding=UTF-8 -classpath /home/_opt/JetBrains/idea-14.0.2-IC-139.659.2/lib/idea_rt.jar:/home/_opt/JetBrains/idea-14.0.2-IC-139.659.2/plugins/junit/lib/junit-rt.jar:/usr/lib/jvm/java-1.8.0-openjdk/jre/lib/jsse.jar:/usr/lib/jvm/java-1.8.0-openjdk/jre/lib/jce.jar:/usr/lib/jvm/java-1.8.0-openjdk/jre/lib/charsets.jar:/usr/lib/jvm/java-1.8.0-openjdk/jre/lib/management-agent.jar:/usr/lib/jvm/java-1.8.0-openjdk/jre/lib/resources.jar:/usr/lib/jvm/java-1.8.0-openjdk/jre/lib/rt.jar:/usr/lib/jvm/java-1.8.0-openjdk/jre/lib/ext/localedata.jar:/usr/lib/jvm/java-1.8.0-openjdk/jre/lib/ext/dnsns.jar:/usr/lib/jvm/java-1.8.0-openjdk/jre/lib/ext/sunjce_provider.jar:/usr/lib/jvm/java-1.8.0-openjdk/jre/lib/ext/sunec.jar:/usr/lib/jvm/java-1.8.0-openjdk/jre/lib/ext/zipfs.jar:/usr/lib/jvm/java-1.8.0-openjdk/jre/lib/ext/cldrdata.jar:/usr/lib/jvm/java-1.8.0-openjdk/jre/lib/ext/sunpkcs11.jar:/usr/lib/jvm/java-1.8.0-openjdk/jre/lib/ext/nashorn.jar:/home/pasha/Projects/ImapTreeSize/target/production/ImapTreeSize:/home/pasha/.groovy/grapes/javax.activation/activation/jars/activation-1.1.jar:/home/pasha/.groovy/grapes/com.sun.mail/gimap/jars/gimap-1.5.2.jar:/home/pasha/.groovy/grapes/com.sun.mail/javax.mail/jars/javax.mail-1.5.2.jar:/home/pasha/.groovy/grapes/org.apache.logging.log4j/log4j-core/jars/log4j-core-2.1.jar:/home/pasha/.groovy/grapes/org.apache.logging.log4j/log4j-api/jars/log4j-api-2.1.jar:/home/pasha/.groovy/grapes/xmlpull/xmlpull/jars/xmlpull-1.1.3.1.jar:/home/pasha/.groovy/grapes/xpp3/xpp3_min/jars/xpp3_min-1.1.4c.jar:/home/pasha/.groovy/grapes/com.thoughtworks.xstream/xstream/jars/xstream-1.4.7.jar:/home/pasha/.m2/repository/org/codehaus/groovy/groovy-all/2.3.7/groovy-all-2.3.7.jar:/home/pasha/.m2/repository/org/apache/ivy/ivy/2.3.0/ivy-2.3.0.jar:/home/pasha/.groovy/grapes/commons-cli/commons-cli/jars/commons-cli-1.2.jar:/home/pasha/.groovy/grapes/junit/junit/jars/junit-4.11.jar:/home/pasha/.groovy/grapes/org.hamcrest/hamcrest-core/jars/hamcrest-core-1.3.jar com.intellij.rt.execution.application.AppMain com.intellij.rt.execution.junit.JUnitStarter -ideVersion5 info.hubbitus.imaptree.utils.bench.SpentTest
+ * Picked up _JAVA_OPTIONS: -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dsun.java2d.pmoffscreen=false -XX:+UseCompressedOops -XX:+DoEscapeAnalysis -XX:+AggressiveOpts -XX:+EliminateLocks
+ * Class not found: "info.hubbitus.imaptree.utils.bench.SpentTest"
+ * Process finished with exit code 1
+ *
+ * And unfortunately JetBrains tracker https://youtrack.jetbrains.com/ seams down:
+ * YouTrack maintenance in progress.
+ * We are currently updating the server.
+ * Sorry for the inconvenience.
+ * This page will be reloaded automatically as soon as YouTrack is ready.
+ * @TODO - report bug when it come online again!
+ *
+ * @author Pavel Alexeev - <Pahan@Hubbitus.info> (pasha)
+ * @created 2015-01-04 19:25
+ * */
+class SpentTest extends GroovyTestCase {
+	void testFormatTimeElapsedSinceNanosecond() {
+
+	}
+
+	void testPlus() {
+
+	}
+
+	void testToString() {
+
+	}
+
+	void testCompareTo() {
+
+	}
+}
